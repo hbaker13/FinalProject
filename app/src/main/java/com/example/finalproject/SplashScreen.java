31e3a8b30d73212932d2237b29777ad87734fb89
Risa <azmi08305@gmail.cpm>
2019-03-20 16:24:29 +0700

    blablabla

--- app/src/main/java/com/example/finalproject/SplashScreen.java
+++ app/src/main/java/com/example/finalproject/SplashScreen.java
@@ -2,13 +2,24 @@ package com.example.finalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashScreen extends AppCompatActivity {
+    private int waktu_loading=5000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent home=new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(home);
+                finish();
+            }
+        },waktu_loading);
     }
 }
