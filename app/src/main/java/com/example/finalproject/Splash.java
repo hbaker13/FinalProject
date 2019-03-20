ad3de238967d893f9cf7b15c03515d2f95c04efe
Khalifa Almaira <khalifaalmaira106@gmail.com>
2019-03-20 13:50:47 +0700

    first commit

---
+++ app/src/main/java/com/example/finalproject/Splash.java
@@ -0,0 +1,22 @@
+package com.example.finalproject;
+
+import android.content.Intent;
+import android.os.Handler;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.widget.TextView;
+
+public class Splash extends AppCompatActivity {
+    TextView tvSplash;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_splash);
+
+        tvSplash = (TextView) findViewById(R.id.tvSplash);
+    }
+}
