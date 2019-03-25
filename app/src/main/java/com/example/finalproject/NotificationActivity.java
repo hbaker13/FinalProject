66fe27da4baa589dd775b3bc69200dabd2d6f1ac
Risa <azmi08305@gmail.cpm>
2019-03-25 10:44:57 +0700

    blablabla

--- app/src/main/java/com/example/finalproject/NotificationActivity.java
+++ app/src/main/java/com/example/finalproject/NotificationActivity.java
@@ -1,8 +1,9 @@
 package com.example.finalproject;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class NotificationActivity extends AppCompatActivity {
 
@@ -10,5 +11,14 @@ public class NotificationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notification);
+    }
+
+    public void saveNotif20(View view) {
+    }
+
+    public void saveNotifExer(View view) {
+    }
+
+    public void saveNotifDrink(View view) {
     }
 }
