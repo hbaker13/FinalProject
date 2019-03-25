a8db3c72ba5ec0b9630554dfc4278cdf12ba23ac
Risa <azmi08305@gmail.cpm>
2019-03-25 11:04:11 +0700

    blablabla

--- app/src/main/java/com/example/finalproject/StartActivity.java
+++ app/src/main/java/com/example/finalproject/StartActivity.java
@@ -1,8 +1,10 @@
 package com.example.finalproject;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class StartActivity extends AppCompatActivity {
 
@@ -10,5 +12,20 @@ public class StartActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+    }
+
+    public void showNotification(View view) {
+        Intent in = new Intent(this, NotificationActivity.class);
+        startActivity(in);
+    }
+
+    public void showSetTime(View view) {
+        Intent ist = new Intent(this, SetTimeActivity.class);
+        startActivity(ist);
+    }
+
+    public void showBacksound(View view) {
+        Intent ibs = new Intent(this, BacksoundActivity.class);
+        startActivity(ibs);
     }
 }
