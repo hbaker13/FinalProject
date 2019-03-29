f9f743dbcaf95b85e1b0e2165968375b7a30fc85
Risa <43493933+Risa835@users.noreply.github.com>
2019-03-29 18:40:01 +0700

    blablabla

--- app/src/main/java/com/example/finalproject/DrinkActivity.java
+++ app/src/main/java/com/example/finalproject/DrinkActivity.java
@@ -10,5 +10,7 @@ public class DrinkActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drink);
+        
+        bye
     }
 }
