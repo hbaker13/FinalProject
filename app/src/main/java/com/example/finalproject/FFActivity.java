4500b034c4ea45f5bc6329ff438e79b61356ea1f
Risa <azmi08305@gmail.cpm>
2019-04-08 23:08:56 +0700

    blablablaaaaa...........

--- app/src/main/java/com/example/finalproject/FFActivity.java
+++ app/src/main/java/com/example/finalproject/FFActivity.java
@@ -1,6 +1,8 @@
 package com.example.finalproject;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +12,10 @@ public class FFActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ff);
+    }
+
+    public void playVideo(View view) {
+        Intent i = new Intent(this, vid.class);
+        startActivity(i);
     }
 }
